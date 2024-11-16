27a4b8f55117d9c0ca301855d1b947f8924d5777
DimataoDexter <sonicdexter12@gamil.com>
2024-11-16 20:19:24 +0800

    added menuhtml

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ class LeafletMap {
         menuItems.forEach(item => {
             menuHtml += `<li data-item="${item.name}" data-price="${item.price}">${item.name}</li>`;
         });
+        menuHtml += "</ul>";
+
+        const popupContent = `
+            <h5>${name}</h5>
+            <p>Menu:</p>
+            ${menuHtml}
+            <div id="price-display" style="margin-top: 10px; font-weight: bold;"></div>
+        `;
